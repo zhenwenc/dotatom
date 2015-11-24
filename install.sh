e9dce5421e5d27ec29f292bbd3be1248466790c2
Frederick Cai <frederick.cai@gmail.com>
2015-11-24 20:19:49 +1300

    update packages

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-echo "Installing all packages..."
-
 PWD=`pwd`
+
+echo "Updating all submodules..."
+
+git submodule foreach git pull origin master
+
+echo "Installing all packages..."
 
 for path in $PWD/packages/* ; do
   [ -d "${path}" ] || continue
